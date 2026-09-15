627a8667a47e590a6505563006770b796434afea
a1stj <tjdlsj0118@gmail.com>
2026-09-15 22:13:36 +0900

    중괄호를 활용한 자바스크립트 표현식을 적용해 동적 렌더링 구현

--- code/03 React Essentials/03-using-outputting-dynamic-values/src/App.jsx
+++ code/03 React Essentials/03-using-outputting-dynamic-values/src/App.jsx
@@ -1,4 +1,4 @@
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -12,6 +12,8 @@ function Header() {
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
+        {/* {reactDescriptions[genRandomInt(2)]} React concepts you will need foralmost any app you are */}
+        {/* 자바스크립트 표현식을 코드밖으로 옮기면 더 깔끔해서 일반적으로 좋은 관례이다. */}
         {description} React concepts you will need for almost any app you are
         going to build!
       </p>
